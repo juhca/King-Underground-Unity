5151c644a55eaddcb0bde335cea67650c6a8393e
delfinaBarisa <delfina.barisa@gmail.com>
2017-01-08 20:52:13 +0100

    Slovenski menu

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+	public Transform mainMenu, instructionsMenu, storyMenu;
+
+	public void LoadScene(string name) {
+		Application.LoadLevel(name);
+	}
+
+	public void QuitGame() {
+		Application.Quit();
+	}
+
+	public void InstructionsMenu(bool clicked) {
+		if(clicked == true) {
+			instructionsMenu.gameObject.SetActive(clicked);
+			mainMenu.gameObject.SetActive(false);
+		}
+		else {
+			instructionsMenu.gameObject.SetActive(clicked);
+			mainMenu.gameObject.SetActive(true);
+		}
+	}
+
+	public void StoryMenu(bool clicked) {
+		if(clicked == true) {
+			storyMenu.gameObject.SetActive(clicked);
+			mainMenu.gameObject.SetActive(false);
+		}
+		else {
+			storyMenu.gameObject.SetActive(clicked);
+			mainMenu.gameObject.SetActive(true);
+		}
+	}
+}

a790eef25598e54e2c314045b6b4f1d651265de0
Tomas <tomas.stefe@gmail.com>
2017-01-09 01:14:27 +0100

    Manjša optimizacija

---
+++ Assets/Scripts/WoodDrag.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WoodDrag : MonoBehaviour {
+
+
+    bool didItPlay = false;
+    private void OnCollisionStay(Collision col)
+    {
+        if(!GetComponent<AudioSource>().isPlaying)
+        {
+            GetComponent<AudioSource>().volume = 1;
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+
+    /*
+    void Update()
+    {
+
+
+        
+        if (GetComponent<Rigidbody>().velocity.magnitude >= 0.1 && !GetComponent<AudioSource>().isPlaying && !didItPlay)
+        {
+            GetComponent<AudioSource>().Play();
+            didItPlay = true;
+        }
+        
+    }*/
+}
